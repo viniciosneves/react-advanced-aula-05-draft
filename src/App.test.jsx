eeb95d3a7e90f5170a703c7888d51eb1ce725304
Vinicios Neves <vinicios.neves@mac>
2024-05-27 13:47:49 +0100

    aula 3

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 import userEvent from '@testing-library/user-event'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+
+// Mock do axios
+const mock = new MockAdapter(axios)
 
 describe('App', () => {
+
+  beforeEach(() => {
+    mock.reset()
+  })
+
   it('renders the App component', () => {
     render(<App />)
     expect(screen.getByText('Cadastro')).toBeInTheDocument();
@@ -52,4 +62,31 @@ describe('App', () => {
     expect(screen.getByText('As senhas devem ser iguais!')).toBeInTheDocument();
   });
 
+
+  test('simulates user input and form submission', async () => {
+    // Define a resposta mockada para a requisição POST
+    mock.onPost('http://localhost:3000/users').reply(200)
+
+    render(<App />);
+
+    const nameInput = screen.getByLabelText('Nome');
+    const emailInput = screen.getByLabelText('E-mail');
+    const passwordInput = screen.getByLabelText('Senha');
+    const confirmPasswordInput = screen.getByLabelText('Confirme sua senha');
+    const submitButton = screen.getByRole('button', { name: /enviar/i });
+
+    // Simula entrada de usuário usando userEvent
+    await userEvent.type(nameInput, 'John Doe');
+    await userEvent.type(emailInput, 'john@example.com');
+    await userEvent.type(passwordInput, 'password123');
+    await userEvent.type(confirmPasswordInput, 'password123');
+
+    // Simula envio do formulário
+    await userEvent.click(submitButton);
+
+    expect(mock.history.post.length).toBe(1)
+    expect(mock.history.post[0].url).toBe('http://localhost:3000/users')
+  });
+
 })
+
